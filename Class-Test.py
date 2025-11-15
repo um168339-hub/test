4a28ec59384890021adfce3f8c162b3e66efdfeb
@Mr.Umar312 <um168339@gmail.com>
2025-11-16 00:37:50 +0500

    Add files via upload

---
+++ Class-Test.py
@@ -0,0 +1,9 @@
+class Temp:
+    def __init__(self):
+        cal = int(input("inter your tamperature: "))
+        kalvan = cal + 237
+        far = cal*9/5 + 32
+        print("kalvan =",kalvan)
+        print("Farhenhite = ",far)
+
+tem1 = Temp()
